560b72ad1f9572c726c180e5591dbcc3231a393f
lukas <hamburgerlukasofficial@gmail.com>
2026-06-30 22:24:00 +0200

    fix

--- src/main/java/at/lukas/projectdashboard/controller/ApiController.java
+++ src/main/java/at/lukas/projectdashboard/controller/ApiController.java
@@ -1,12 +1,17 @@
 package at.lukas.projectdashboard.controller;
+
 import at.lukas.projectdashboard.FileHelper;
+import at.lukas.projectdashboard.GitManager;
 import at.lukas.projectdashboard.data.TotalDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,6 +20,13 @@ import java.nio.file.Paths;
 @RequestMapping("/api")
 public class ApiController
 {
+    private final GitManager gitManager;
+
+    public ApiController(GitManager gitManager)
+    {
+        this.gitManager = gitManager;
+    }
+
     @Value("${github.repo.path}")
     private String repopath;
 
@@ -26,6 +38,20 @@ public class ApiController
     public TotalDto getAllFiles()
     {
         Path repoPath = Paths.get(repopath).resolve(analisedir);
+
+        if (!Files.isDirectory(repoPath))
+        {
+            log.info("Repository path {} does not exist, updating repository", repoPath);
+            gitManager.update();
+        }
+
+        if (!Files.isDirectory(repoPath))
+        {
+            throw new ResponseStatusException(
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    "Configured analysis path does not exist: " + repoPath
+            );
+        }
 
         log.info("Received request");
         return FileHelper.getFiles(repoPath);
